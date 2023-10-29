c231d6b2541464d4e7e73ae42a0c56e12ec3c9e4
pengxiang li <pl7@email.sc.edu>
2023-10-29 17:26:48 -0400

    change code style to black

--- project490/Custom/apps.py
+++ project490/Custom/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CustomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Custom'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Custom"
