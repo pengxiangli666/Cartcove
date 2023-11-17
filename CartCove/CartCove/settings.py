a73aa0bdb0c6a93cdebf54da0a183cb389a06d66
Ultragum <jamelc2002@gmail.com>
2023-11-17 14:21:23 -0500

    Got React Router to work

--- CartCove/CartCove/settings.py
+++ CartCove/CartCove/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,16 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+##
+
+# Add this if your static files are stored in the 'static' directory of your Django app
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/static/frontend/main.js')]
+
+# Add this to collect static files to a common location
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+##
 
 # Application definition
 
