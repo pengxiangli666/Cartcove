843dc958596ee51eddac2a9cd3caeda87694ce79
pengxiang li <pl7@email.sc.edu>
2023-11-19 20:07:28 -0500

    create media folder to save products image

--- CartCove/CartCove/urls.py
+++ CartCove/CartCove/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from cart.views import ProductViewSet
 from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
 
 # 创建路由器并注册你的视图集
 router = DefaultRouter()
@@ -30,3 +32,6 @@ urlpatterns = [
     path("cart/", include("cart.urls")),  # 购物车应用的 URL
     path("api/", include(router.urls)),  # 将 API 路径设置为 /api/
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
