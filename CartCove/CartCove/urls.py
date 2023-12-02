559ea9dbb077e011ea3c94d8efee88f670a34ab0
pengxiang li <pl7@email.sc.edu>
2023-12-02 15:24:47 -0500

    fix error

--- CartCove/CartCove/urls.py
+++ CartCove/CartCove/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework.routers import DefaultRouter
-from rest_framework import permissions
+from cart.views import ProductViewSet, CartItemViewSet  # Make sure to import CartItemViewSet
+from frontend.views import index
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
 
+# Create a router and register your view set
+router = DefaultRouter()
+router.register(r"products", ProductViewSet)  # Used for processing product-related API request
+router.register(
+    r"cart-items", CartItemViewSet, basename="cartitem"
+)  # Used to process items related to shopping carts API request
 
 from cart.views import (
     ProductViewSet,
@@ -37,32 +47,18 @@ router.register(r"products", ProductViewSet)
 router.register(r"cart-items", CartItemViewSet, basename="cartitem")
 
 urlpatterns = [
-    # Set the root URL to point to the Swagger UI
-    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    path("admin/", admin.site.urls),  # management interface
-    path("auth/", include("dj_rest_auth.urls")),  # Authentication-related URLs
-    path(
-        "auth/registration/", include("dj_rest_auth.registration.urls")
-    ),  # Register the relevant URL
-    path("cart/", include("cart.urls")),  # URL of the shopping cart application
-    # Your existing API path
-    path("api/search/", search_products, name="search_products"),
-    path("api/add-to-cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
-    path("api/cart-items/", view_cart_items, name="view_cart_items"),
-    path(
-        "api/remove-from-cart/<int:product_id>/",
-        remove_from_cart,
-        name="remove_from_cart",
-    ),
-    # DRF URL
-    path("api/", include(router.urls)),  # Set the API path to /api/
-    # Other Swagger and Redoc related URLs
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
+    path("", index),  # Used to handle front-end applications URL
+    path("SignIn", index),  
+    path("Register", index),  
+    path("PersonalSettings", index),  
+    path("Detail", index),  
+    path("admin/", admin.site.urls),  
+    path("auth/", include("dj_rest_auth.urls")),  # authentication-related URL
+    path("auth/registration/", include("dj_rest_auth.registration.urls")),  # registration-related URL
+    path("cart/", include("cart.urls")),  # Shopping cart application URL
+    path("api/", include(router.urls)),  # API Path set to /api/
+
 ]
 
 if settings.DEBUG:
