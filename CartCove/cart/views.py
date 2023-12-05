26c68701908adbfcc0d2908b2a1914e868b1b7a9
pengxiang li <pl7@email.sc.edu>
2023-12-04 23:53:34 -0500

    api for review

--- CartCove/cart/views.py
+++ CartCove/cart/views.py
@@ -7,7 +7,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 import json
-
+from rest_framework import permissions, viewsets
+from .models import Review
+from .serializers import ReviewSerializer
 from .models import Product, CartItem
 from .serializers import ProductSerializer, CartItemSerializer
 
@@ -76,3 +78,11 @@ class CartItemViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
