1b57b4af400683216f05cf710f0309606e3a0a09
pengxiangli666 <pl7@email.sc.edu>
2024-01-13 03:01:15 -0500

    v0.01

--- CartCove/cart/views.py
+++ CartCove/cart/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.http import require_POST
 from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -65,7 +65,7 @@ def create_product(request):
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=400)
 
-
+@permission_classes([AllowAny])
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
