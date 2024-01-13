054227e92148b20fe213dd68c164333709bb03ae
pengxiangli666 <pl7@email.sc.edu>
2024-01-12 23:49:50 -0500

    renew fe

--- CartCove/frontend/webpack.config.js
+++ CartCove/frontend/webpack.config.js
@@ -11,28 +11,29 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        exclude: /node_modules/,
         use: ["style-loader", "css-loader"],
       },
-
       {
-        test: /\.tsx$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-        },
-      },
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+          }
+        }
+      }
     ],
-  },
-  optimization: {
-    minimize: true,
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development"),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "production"),
     }),
   ],
+  optimization: {
+    minimize: true,
+  },
 };
