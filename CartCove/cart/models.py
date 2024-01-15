9d881b7e8f709900165899f74386bb54eab64386
pengxiangli666 <pl7@email.sc.edu>
2024-01-15 05:44:46 -0500

    renew model

--- CartCove/cart/models.py
+++ CartCove/cart/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(
         upload_to="products/", blank=True, null=True
-    )  # Add image field
+    )  
     ##description = models.TextField(blank=True)  
 
     def __str__(self):
