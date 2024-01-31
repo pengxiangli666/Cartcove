4d60e3539916c4d6fbbaa2b3fa6f8ca1c990f2b0
pengxiangli666 <pl7@email.sc.edu>
2024-01-31 02:58:14 -0500

    renew

--- CartCove/cart/urls.py
+++ CartCove/cart/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("api/add-to-cart/<int:product_id>/", add_to_cart, name="add_to_cart"),
     path("api/cart-items/", view_cart_items, name="view_cart_items"),
     path(
-        "api/remove-from-cart/",
+        "api/remove-from-cart",
         remove_from_cart,
         name="remove_from_cart",
     ),
