ca7b8992df317e851d1dd8ca6a77dffce1fa90ca
pengxiangli666 <pl7@email.sc.edu>
2024-02-12 02:49:18 -0500

    make a categroy

--- CartCove/cart/models.py
+++ CartCove/cart/models.py
@@ -3,17 +3,30 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+    
+
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(
         upload_to="products/", blank=True, null=True
     )  
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
     #description = models.TextField(blank=True)  
 
     def __str__(self):
         return self.name
-
+    
 
 class CartItem(models.Model):
     user = models.ForeignKey(
