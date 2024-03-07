d0838d584e34187ae632b0a91364fa077ef142ee
pengxiangli666 <pl7@email.sc.edu>
2024-03-07 01:55:50 -0500

    re

--- CartCove/CartCove/settings.py
+++ CartCove/CartCove/settings.py
@@ -106,9 +106,9 @@ WSGI_APPLICATION = "CartCove.wsgi.application"
 
 DATABASES = {
     "default": {
-        #"ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": "/app/db.sqlite3", # for docker
-        "NAME": "db.sqlite3", # for local
+        #"NAME": "db.sqlite3", # for local
     }
 }
 
